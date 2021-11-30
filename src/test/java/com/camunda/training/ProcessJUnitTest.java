afcc91c01b0c345cff2ca73b488f2b04c7f3019c
MichaelP <michael-czaja-arbeit@hotmail.de>
2021-11-30 15:03:44 +0100

    Lecture 5, - Removed unneeded imports

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,19 +1,15 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
 import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(ProcessEngineCoverageExtension.class)
 public class ProcessJUnitTest {
