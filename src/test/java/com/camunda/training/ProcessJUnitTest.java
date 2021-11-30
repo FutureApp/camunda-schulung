aeaf77d81eeae986ef90deccf154211e88ab8e0b
MichaelP <michael-czaja-arbeit@hotmail.de>
2021-11-30 14:33:04 +0100

    Lecture 5, - Check Camunda is running, find task, handle user-task

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,6 +1,7 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
 import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
@@ -18,13 +20,28 @@ public class ProcessJUnitTest {
 
     @Test
     @Deployment(resources = "Process_TwitterQA.bpmn")
-    public void testEngine() {
+    public void happyPath() {
         // Create a HashMap to put in variables for the process instance
         Map<String, Object> variables = new HashMap<String, Object>();
-        variables.put("approved", true);
         // Start process with Java API and variables
         ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
         // Make assertions on the process instance
+        String msgPruefenTaskId = findId("Nachricht prüfen"); // Sucht die ID via den Namen
+
+        List<Task> taskList = taskService()
+                .createTaskQuery()
+                .taskCandidateGroup("management")
+                .processInstanceId(processInstance.getId())
+                .list();
+
+        assertThat(taskList).isNotNull();
+        assertThat(taskList).hasSize(1);
+        Task task = taskList.get(0);
+
+        Map<String, Object> approvedMap = new HashMap<>();
+        approvedMap.put("approved", true);
+        taskService().complete(task.getId(), approvedMap); // Führt den Task zu Ende
+
         assertThat(processInstance).isEnded();
     }
 }
