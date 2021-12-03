d6bc5e4ce9a0102e72488eade61ab2eba79c0b4b
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2021-12-03 14:47:07 +0100

    Lecture Dec-Table, - Decision-Table, no work

---
+++ src/test/java/com/camunda/training/DMNTest.java
@@ -0,0 +1,35 @@
+package com.camunda.training;
+
+import org.camunda.bpm.dmn.engine.DmnDecisionRuleResult;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.engine.runtime.Job;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
+import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.decisionService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.execute;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.externalTask;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.findId;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.jobQuery;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.withVariables;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(ProcessEngineCoverageExtension.class)
+public class DMNTest {
+
+
+
+
+}
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,6 +1,8 @@
 package com.camunda.training;
 
 import org.assertj.core.api.Assert;
+import org.camunda.bpm.dmn.engine.DmnDecisionRuleResult;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.engine.runtime.Job;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
@@ -15,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.decisionService;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.execute;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.externalTask;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.findId;
@@ -27,11 +30,12 @@ import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.withVar
 public class ProcessJUnitTest {
 
     @Test
-    @Deployment(resources = "Process_TwitterQA.bpmn")
+    @Deployment(resources = {"Process_TwitterQA.bpmn", "tweetApproval.dmn"})
     public void happyPath() {
         // Create a HashMap to put in variables for the process instance
         Map<String, Object> variables = new HashMap<>();
         variables.put("approved", true);
+        variables.put("email", "can.tweet@camunda.org");
         variables.put("content", "Bob True");
         // Start process with Java API and variables
         ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
@@ -114,4 +118,19 @@ public class ProcessJUnitTest {
         BpmnAwareTests.assertThat(processInstance).isEnded();
     }
 
+
+
+    @Test
+    @Deployment(resources = {"tweetApproval.dmn", "Process_TwitterQA.bpmn"})
+    public void testDecisionTable() {
+
+        Map<String, Object> variables = withVariables(
+                "email", "can.tweet@camunda.org",
+                "content", "this should be published");
+        DmnDecisionTableResult decisionResult = decisionService().evaluateDecisionTableByKey("Decision_tweetApproval", variables);
+
+        DmnDecisionRuleResult firstResult = decisionResult.getFirstResult();
+        Boolean approved = (Boolean)firstResult.getEntryMap().get("approved");
+        Assertions.assertEquals(approved, true);
+    }
 }
